f60442f54c7e3334406b0dd370fc04d228300074
geschuhbi <geschuhbi14@gmail.com>
2023-10-13 11:16:30 +0200

    update

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -1,10 +1,10 @@
-WITH temp_daily AS (
+WITH temperature_daily AS (
     SELECT * 
     FROM {{ref('staging_temp')}}
 ),
 add_weekday AS (
     SELECT *,
-        to_char(date, 'Dy') AS weekday, 
+        to_char(date, 'Day') AS weekday, 
         to_char(date, 'DD') AS day_num 
     FROM temp_daily
 )
