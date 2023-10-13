d94005a379b217d183c7ab2b5129dabf70fef05b
geschuhbi <geschuhbi14@gmail.com>
2023-10-13 13:57:46 +0200

    2 weekday

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,12 +1,12 @@
-with total_avg as (
-    select 
-        city, country, year, month, week
-         AVG(avgtemp_c) AS avg_temp_week,
-         MAX(maxtemp_c) AS max_temp_week,
-         MIN(mintemp_c) AS min_temp_week
-    from {{ref('prep_temp')}}
-    group by week, year, month, country, city
+WITH total_avg AS (
+    SELECT 
+        city, country, year, weekday,
+        AVG(avgtemp_c) AS avg_temp_weekday,
+        MAX(maxtemp_c) AS max_temp_weekday,
+        MIN(mintemp_c) AS min_temp_weekday
+    FROM {{ref('prep_temp')}}
+    GROUP BY city, country, year, weekday
 )
 
-select *
-from total_avg
+SELECT *
+FROM total_avg
