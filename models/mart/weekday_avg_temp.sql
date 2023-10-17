075aa99a5d714aadaf324ccb86e0eac6ee0ca53a
geschuhbi <geschuhbi14@gmail.com>
2023-10-17 15:57:13 +0200

    2

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,12 +1,35 @@
-WITH total_avg AS (
+WITH weekly_avg AS (
     SELECT 
-        city, country, year, weekday,
-        AVG(avgtemp_c) AS avg_temp_weekday,
-        MAX(maxtemp_c) AS max_temp_weekday,
-        MIN(mintemp_c) AS min_temp_weekday
+        city, country, year, week,
+        AVG(avgtemp_c) AS avg_temp_week,
+        MAX(maxtemp_c) AS max_temp_week,
+        MIN(mintemp_c) AS min_temp_week
     FROM {{ref('prep_temp')}}
-    GROUP BY city, country, year, weekday
+    GROUP BY city, country, year, week
+),
+monthly_avg AS (
+    SELECT 
+        city, country, year, month,
+        AVG(avgtemp_c) AS avg_temp_month,
+        MAX(maxtemp_c) AS max_temp_month,
+        MIN(mintemp_c) AS min_temp_month
+    FROM {{ref('prep_temp')}}
+    GROUP BY city, country, year, month
 )
-
-SELECT *
-FROM total_avg
+SELECT 
+    city, country, year, weekday,
+    AVG(avgtemp_c) AS avg_temp_weekday,
+    MAX(maxtemp_c) AS max_temp_weekday,
+    MIN(mintemp_c) AS min_temp_weekday
+FROM {{ref('prep_temp')}}
+GROUP BY city, country, year, weekday
+UNION ALL
+SELECT 
+    city, country, year, week, NULL AS weekday,
+    avg_temp_week, max_temp_week, min_temp_week
+FROM weekly_avg
+UNION ALL
+SELECT 
+    city, country, year, month, NULL AS weekday,
+    avg_temp_month, max_temp_month, min_temp_month
+FROM monthly_avg;
