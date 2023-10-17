8976f2c4bd978d0fdfa67f8b2178bdfb29b6bc66
geschuhbi <geschuhbi14@gmail.com>
2023-10-17 16:23:10 +0200

    6

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,32 +1,11 @@
-WITH weekly_avg AS (
-    SELECT 
-        city, country, year, week,
-        AVG(avgtemp_c) AS avg_temp_week,
-        MAX(maxtemp_c) AS max_temp_week,
-        MIN(mintemp_c) AS min_temp_week
+WITH total_avg AS (
+    SELECT
+        city, country, year, weekday,
+        AVG(avgtemp_c) AS avg_temp_weekday,
+        MAX(maxtemp_c) AS max_temp_weekday,
+        MIN(mintemp_c) AS min_temp_weekday
     FROM {{ref('prep_temp')}}
-    GROUP BY city, country, year, week
-),
-monthly_avg AS (
-    SELECT 
-        city, country, year, month,
-        AVG(avgtemp_c) AS avg_temp_month,
-        MAX(maxtemp_c) AS max_temp_month,
-        MIN(mintemp_c) AS min_temp_month
-    FROM {{ref('prep_temp')}}
-    GROUP BY city, country, year, month
-)
-SELECT 
-    t.city, t.country, t.year, t.weekday,
-    AVG(t.avgtemp_c) AS avg_temp_weekday,
-    MAX(t.maxtemp_c) AS max_temp_weekday,
-    MIN(t.mintemp_c) AS min_temp_weekday,
-    w.avg_temp_week, w.max_temp_week, w.min_temp_week,
-    m.avg_temp_month, m.max_temp_month, m.min_temp_month
-FROM {{ref('prep_temp')}} AS t
-LEFT JOIN weekly_avg AS w USING (city, country, year, week)
-LEFT JOIN monthly_avg AS m USING (city, country, year, month)
-GROUP BY t.city, t.country, t.year, t.weekday
+    GROUP BY city, country, year, weekday
 )
 SELECT *
-FROM weekly_avg
+FROM total_avg
