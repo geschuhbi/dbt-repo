19d5ebfa738646fa4b93ebd0e356834c08e809f9
geschuhbi <geschuhbi14@gmail.com>
2023-10-18 18:42:55 +0200

    eso

--- models/mart/monthly_avg_temp.sql
+++ models/mart/monthly_avg_temp.sql
@@ -1,11 +1,11 @@
 WITH month_avg AS (
     SELECT
-        city, country, year, month, lat, lon,
+        city, country, continent, year, month, lat, lon,
         AVG(avgtemp_c) AS avg_temp_monthly,
         MAX(maxtemp_c) AS max_temp_monthly,
         MIN(mintemp_c) AS min_temp_monthly
     FROM {{ref('prep_temp')}}
-    GROUP BY city, country, year, month, lat, lon
+    GROUP BY city, country, continent, year, month, lat, lon
 )
 SELECT *
 FROM month_avg
