9fb36b84b45a79e0d70503f793a3afc8963630ed
geschuhbi <geschuhbi14@gmail.com>
2023-10-19 14:53:18 +0200

    num

--- models/mart/monthly_avg_temp.sql
+++ models/mart/monthly_avg_temp.sql
@@ -1,6 +1,6 @@
 WITH month_avg AS (
     SELECT
-        city, country,year, month, month_num lat, lon,
+        city, country,year, month, month_num, lat, lon,
         AVG(avgtemp_c) AS avg_temp_monthly,
         MAX(maxtemp_c) AS max_temp_monthly,
         MIN(mintemp_c) AS min_temp_monthly,
