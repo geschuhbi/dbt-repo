94c52571cf6fdae5f8f5e214fa935c05a56fa8a9
geschuhbi <geschuhbi14@gmail.com>
2023-10-19 10:58:54 +0200

    month

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -5,8 +5,9 @@ WITH temperature_daily AS (
 add_datetime_info AS (
     SELECT *,
         to_char(date, 'Day') AS weekday, 
-        to_char(date, 'DD') AS day_num, 
-        to_char(date, 'Month') AS month,
+        to_char(date, 'DD') AS day_num,
+        to_char(date, 'MM') AS month, 
+        to_char(date, 'Month') AS month_l,
         to_char(date, 'IYYY') AS year,
         to_char(date, 'WW') AS week  
     FROM temperature_daily
